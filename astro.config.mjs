637348efa338b002143ee19f5fc2a585da3850bd
Sylvia Crowe <software@oneirocosm.com>
2025-07-03 14:43:41 -0700

    feat: create basic project from template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			plugins: [starlightThemeRapide()],
+			title: 'ARGroup Intranet',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
